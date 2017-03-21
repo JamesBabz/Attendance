4ea3ce2737b7893bd46244f3cfcac212fcbee05a
Enemark96 <je.enemark96@gmail.com>
2017-03-21 13:15:11 +0100

    Vælg klasse og dato på teacherview
    Vælg klasse og dato på teacherview

--- src/attendance/gui/controller/TeacherViewController.java
+++ src/attendance/gui/controller/TeacherViewController.java
@@ -19,8 +19,11 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.Set;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -49,14 +52,13 @@ import javafx.scene.control.Alert;
  *
  * @author Simon Birkedal, Stephan Fuhlendorff, Thomas Hansen & Jacob Enemark
  */
-public class TeacherViewController extends Dragable implements Initializable
-{
+public class TeacherViewController extends Dragable implements Initializable {
 
     private final TeacherModel model;
     private final PersonManager manager;
     private final DateTimeModel dateTimeModel;
     private final List<Student> studentList;
-    
+
     private final ObservableList<Student> allStudents;
     private ObservableList<Student> searchedStudents;
     private final List<Absence> absence;
@@ -93,8 +95,7 @@ public class TeacherViewController extends Dragable implements Initializable
     /**
      * The default constructor for the TeacherViewController.
      */
-    public TeacherViewController() throws SQLException, IOException
-    {
+    public TeacherViewController() throws SQLException, IOException {
         this.manager = new PersonManager();
         this.studentList = manager.getAllStudents();
         this.allStudents = FXCollections.observableArrayList();
@@ -103,106 +104,85 @@ public class TeacherViewController extends Dragable implements Initializable
         dateTimeModel = new DateTimeModel();
         allStudents.addAll(studentList);
         absence = new ArrayList<>();
-        
+
     }
 
     /**
      * Initializes the controller class.
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb)
-    {
+    public void initialize(URL url, ResourceBundle rb) {
         lblUsername.setText(model.getCurrentUser().getFirstName() + " " + model.getCurrentUser().getLastName());
         tblStudentAbs.setItems(allStudents);
         colStudent.setCellValueFactory(new PropertyValueFactory<>("fullName"));
 
         addCheckBoxes();
 
-        fillComboBoxes();
+     
         updateDateInfo();
         search();
         setLogo();
         setSemester();
-
+        setClass();
+        
     }
 
-    public void getAllAbsence(LocalDate startDate, LocalDate endDate) throws SQLException
-    {
+    public void getAllAbsence(LocalDate startDate, LocalDate endDate) throws SQLException {
         absence.clear();
         absence.addAll(manager.getAllAbsence(startDate, endDate));
-        for (Absence abs : absence)
-        {
+        for (Absence abs : absence) {
             System.out.println(abs.getDate().toString());
         }
     }
 
-    public void handleFirstDate() throws SQLException
-    {
+    public void handleFirstDate() throws SQLException {
         firstDate = dateFirstDate.getValue();
-        if (dateSecondDate.getValue() != null)
-        {
+        if (dateSecondDate.getValue() != null) {
             getAllAbsence(firstDate, secondDate);
 
         }
 
     }
 
-    public void handleSecondDate() throws SQLException
-    {
+    public void handleSecondDate() throws SQLException {
         secondDate = dateSecondDate.getValue();
-        if (dateFirstDate.getValue() != null)
-        {
+        if (dateFirstDate.getValue() != null) {
             getAllAbsence(firstDate, secondDate);
         }
 
     }
 
     @FXML
-    private void closeWindow(MouseEvent event)
-    {
+    private void closeWindow(MouseEvent event) {
         Stage stage = (Stage) closeButton.getScene().getWindow();
         stage.close();
 
     }
 
     @FXML
-    private void setOffset(MouseEvent event)
-    {
+    private void setOffset(MouseEvent event) {
         startDrag(event);
     }
 
-    private void fillComboBoxes()
-    {
-        comboClass.getItems().add("CS2016A");
-        comboClass.getItems().add("CS2016B");
-
-        getCurrentDate();
-    }
-
-    private void getCurrentDate()
-    {
+    private void getCurrentDate() {
 //        dateFirstDate.setValue(LocalDate.now());
 //        dateSecondDate.setValue(LocalDate.now());
 
     }
 
-    private void updateDateInfo()
-    {
+    private void updateDateInfo() {
 
     }
 
-    private void search()
-    {
+    private void search() {
         txtSearch.textProperty().addListener((ObservableValue<? extends String> listener, String oldQuery, String newQuery)
-                ->
-        {
+                -> {
             searchedStudents.setAll(model.search(studentList, newQuery));
             tblStudentAbs.setItems(searchedStudents);
         });
     }
 
-    private void setLogo()
-    {
+    private void setLogo() {
         Image imageEasv = new Image("attendance/gui/view/images/easv.png");
         imageLogo.setImage(imageEasv);
         imageLogo.setFitHeight(80);
@@ -210,21 +190,17 @@ public class TeacherViewController extends Dragable implements Initializable
     }
 
     @FXML
-    private void drag(MouseEvent event)
-    {
+    private void drag(MouseEvent event) {
         dragging(event, txtSearch);
     }
 
-    private void addCheckBoxes()
-    {
+    private void addCheckBoxes() {
 
         colAbsence.setCellValueFactory(
-                new Callback<CellDataFeatures<Student, Boolean>, ObservableValue<Boolean>>()
-        {
+                new Callback<CellDataFeatures<Student, Boolean>, ObservableValue<Boolean>>() {
 
             @Override
-            public ObservableValue<Boolean> call(CellDataFeatures<Student, Boolean> param)
-            {
+            public ObservableValue<Boolean> call(CellDataFeatures<Student, Boolean> param) {
                 return param.getValue().registeredProperty();
             }
         });
@@ -233,22 +209,17 @@ public class TeacherViewController extends Dragable implements Initializable
     }
 
     @FXML
-    private void changeStudentAbsence()
-    {
+    private void changeStudentAbsence() {
         selectedStudent = tblStudentAbs.getSelectionModel().getSelectedItem();
 
-        if (selectedStudent.isRegistered())
-        {
+        if (selectedStudent.isRegistered()) {
 
             selectedStudent.setLastCheckOut(Timestamp.from(Instant.now()));
             selectedStudent.setLastCheckIn(null);
             selectedStudent.setRegistered(false);
             updateCheckInAndOut();
-        } else
-
-        {
-            if (selectedStudent.getLastCheckOut() == null)
-            {
+        } else {
+            if (selectedStudent.getLastCheckOut() == null) {
                 selectedStudent.setLastCheckOut(Timestamp.from(Instant.now().minusSeconds(1)));
 
                 updateCheckInAndOut();
@@ -260,43 +231,63 @@ public class TeacherViewController extends Dragable implements Initializable
         }
     }
 
-    private void updateCheckInAndOut()
-    {
-        try
-        {
+    private void updateCheckInAndOut() {
+        try {
             manager.updateCheckIn(selectedStudent);
             manager.updateCheckOut(selectedStudent);
-        } catch (SQLException ex)
-        {
+        } catch (SQLException ex) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Connection to database lost");
-                    
+
         }
     }
-    
-    private void setSemester()
-    {
-        
-    comboSemester.getItems().addAll(
-            "1. Semester",
-            "2. Semester",
-            "3. Semester",
-            "4. Semester"  
-);
-    
-    
+
+    private void setSemester() {
+
+        comboSemester.getItems().addAll(
+                "1. Semester",
+                "2. Semester",
+                "3. Semester",
+                "4. Semester"
+        );
+    }
+
+    private void setClass() {
+        comboClass.getItems().addAll(
+                getAllClassNames()
+        );
+    }
+
+    private List<String> getAllClassNames() {
+        List<String> classNames = new ArrayList<>();
+        for (Student student : studentList) {
+            classNames.add(student.getClassName());
+        }
+
+        Set<String> distinctClassNames = new HashSet<>(classNames);
+        classNames.clear();
+        classNames.addAll(distinctClassNames);
+        classNames.removeAll(Collections.singleton(null));
+        Collections.sort(classNames);
+        return classNames;
     }
 
     @FXML
     private void handleSemesterSelect(ActionEvent event) {
-       int semesterNum = comboSemester.getSelectionModel().getSelectedIndex() +1;
-       Semester semester = new Semester(semesterNum, comboClass.getValue());
-       
-       LocalDate firstDate = semester.getStartDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-       LocalDate secondDate = semester.getEndDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-       
-       dateFirstDate.setValue(firstDate);
-       dateSecondDate.setValue(secondDate);
+        
+        if(comboClass.getSelectionModel().getSelectedIndex() != -1){
+        int semesterNum = comboSemester.getSelectionModel().getSelectedIndex() + 1;
+        Semester semester = new Semester(semesterNum, comboClass.getValue());
+        
+        LocalDate firstDate = semester.getStartDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        LocalDate secondDate = semester.getEndDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
 
+        dateFirstDate.setValue(firstDate);
+        dateSecondDate.setValue(secondDate);
+
+        }
+
+  
     }
+
 }
