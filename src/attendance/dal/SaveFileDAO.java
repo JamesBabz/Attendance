91a64ffdb40205d4112fda16da8437be7cdbbefd
JamesBabz <web.fuhlendorff@gmail.com>
2017-03-22 10:13:51 +0100

    added en- & decrypting
    added en- & decrypting when saving and loading from the file

--- src/attendance/dal/SaveFileDAO.java
+++ src/attendance/dal/SaveFileDAO.java
@@ -5,16 +5,23 @@
  */
 package attendance.dal;
 
-import attendance.be.Person;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
+import java.util.Base64;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 
 /**
  *
@@ -23,42 +30,67 @@ import java.util.Scanner;
 public class SaveFileDAO
 {
 
-
-
     public SaveFileDAO(String fileName)
     {
 
     }
 
-    public void saveLogin(String userName, String passWord) throws IOException
+    public void saveLogin(String userName, String passWord) throws IOException, UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException
     {
+
         try (BufferedWriter bw = new BufferedWriter(new FileWriter("LoginData.txt")))
         {
-            
-            bw.append(userName 
+            userName = encrypt(userName);
+            passWord = encrypt(passWord);
+            bw.append(userName
                     + ", " + passWord);
-         
+
         }
     }
 
-    public String[] loadLogin() throws FileNotFoundException, IOException
+    public String[] loadLogin() throws FileNotFoundException, IOException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException
     {
 //        ArrayList<String> userData = new ArrayList<>();
         String[] loadedArray = new String[2];
 
-       try (BufferedReader br = new BufferedReader(new FileReader("LoginData.txt")))
+        try (BufferedReader br = new BufferedReader(new FileReader("LoginData.txt")))
         {
             Scanner scanner = new Scanner(br);
             while (scanner.hasNext())
             {
-                     String line = scanner.nextLine();
+                String line = scanner.nextLine();
                 String[] fields = line.split(",");
                 loadedArray[0] = fields[0].trim();
                 loadedArray[1] = fields[1].trim();
-            
-        
+
             }
         }
+        loadedArray[0] = decrypt(loadedArray[0]);
+        loadedArray[1] = decrypt(loadedArray[1]);
         return loadedArray;
+    }
+
+    public static String encrypt(String text) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException
+    {
+
+        String key = "Bar12345Bar12345";
+        Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+        byte[] encrypted = cipher.doFinal(text.getBytes());
+        Base64.Encoder encoder = Base64.getEncoder();
+        String encryptedString = encoder.encodeToString(encrypted);
+        return encryptedString;
+    }
+
+    public static String decrypt(String encryptedString) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException
+    {
+        String key = "Bar12345Bar12345";
+        Key aesKey = new SecretKeySpec(key.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        Base64.Decoder decoder = Base64.getDecoder();
+        cipher.init(Cipher.DECRYPT_MODE, aesKey);
+        String decrypted = new String(cipher.doFinal(decoder.decode(encryptedString)));
+        return decrypted;
     }
 }
