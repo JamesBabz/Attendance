2b2b337c7f29667d9242d37e95f7d7ff328217ce
thomasmeyerh <thom953b@easv365.dk>
2017-04-06 12:48:01 +0200

    JavaDoc to DAL and BLL
    JavaDoc to DAL and BLL

--- src/attendance/bll/SaveFileManager.java
+++ src/attendance/bll/SaveFileManager.java
@@ -9,26 +9,37 @@ import attendance.dal.SaveFileDAO;
 import java.io.IOException;
 
 /**
- *
- * @author thomas
+ * @author Simon Birkedal, Stephan Fuhlendorff, Thomas Hansen & Jacob Enemark
  */
 public class SaveFileManager
 {
     private SaveFileDAO saveFileDAO;
 
+/**
+ * Contructor.
+ */
     public SaveFileManager()
     {
         saveFileDAO = new SaveFileDAO("LoginData.txt");
     }
     
-    
+/**
+ * Sends the login data further to SaveFileDAO.
+ * @param userName String to be saved
+ * @param passWord String to be saved
+ * @throws IOException 
+ */
     public void saveLoginData(String userName, String passWord) throws IOException 
     {
         
             saveFileDAO.saveLogin(userName, passWord);
   
     }
-    
+    /**
+     * Loads the login data from SaveFiledAO
+     * @return String list with login data
+     * @throws IOException 
+     */
     public String[] loadLoginData() throws IOException 
     {
         
